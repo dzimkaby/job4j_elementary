b5f0cbd307203b4911e92fca6a27f7aac5581417
dzimkaby <dzimkaby@gmail.com>
2020-04-15 17:39:33 +0300

    Загрузить Counter.java c изменениями в github

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -1,21 +1,32 @@
 package ru.job4j.loop;
 
 public class Counter {
+
     public static int sum(int start, int finish) {
-        int sum = 0;
-        for ( int index = start; index <= finish; index++  ) {
-            sum = sum + index;
+        int sum1 = 0;
+        for (int index = start; index <= finish; index++) {
+            sum1 = sum1 + index;
         }
-        return sum;
+        return sum1;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(0, 20));
-        System.out.println(sum(1, 20));
-        System.out.println(sum(2, 20));
-        System.out.println(sum(3, 20));
+    public static int sumByEven(int start, int finish) {
+        int sum2 = 0;
+        for (int index = start; index <= finish; index++) {
+            if (index % 2 == 0) {
+                sum2 = sum2 + index;
+            }
+        }
+        return sum2;
     }
-}
+
+        public static void main(String[] args) {
+            System.out.println(sum(0, 10));
+            System.out.println(sum(3, 8));
+            System.out.println(sum(1, 1));
+
+            System.out.println(sumByEven(0, 10));
+            System.out.println(sumByEven(3, 8));
+            System.out.println(sumByEven(1, 1));
+        }
+    }
