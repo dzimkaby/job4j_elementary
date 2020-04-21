5356bef006904cf4d19d72fb76b7a832b0ff786a
dzimkaby <dzimkaby@gmail.com>
2020-04-21 15:19:44 +0300

    Загрузить FindLoop.java и FindLoopTest.java в github

---
+++ src/ru/job4j/array/FindLoop.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int result = FindLoop.indexOf(new int[] {2, 4, 6, 8, 10}, 6);
+        System.out.println(result);
+    }
+}
---
+++ src/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+    import org.junit.Test;
+    import static org.hamcrest.core.Is.is;
+    import static org.junit.Assert.assertThat;
+public class FindLoopTest {
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas2Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int value = 2;
+        int result = find.indexOf(input, value);
+        int expect = 1;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHasNo2ThenNo() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {1, 3, 5, 7, 9};
+        int value = 2;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
