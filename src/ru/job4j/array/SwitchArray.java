472d794257f70aca512857de50793c52b9302701
dzimkaby <dzimkaby@gmail.com>
2020-04-23 09:34:46 +0300

    Загрузить SwitchArray.java в github

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -1,18 +1,35 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+        int temp = dest;
+        dest = source;
+        source = temp;
+        return array;
+    }
+/**
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[(array.length - 1)];
         array[(array.length-1)] = temp;
         return array;
     }
-
+**/
     public static void main(String[] args) {
+        /**
         int[] nums = new int[] {1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
         }
+        System.out.println();
+         **/
+
+        int[] nums2 = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rsl2 = swap(nums2, 1, 4);
+        for (int index = 0; index < rsl2.length; index++) {
+            System.out.println(rsl2[index]);
+        }
+
     }
 }
