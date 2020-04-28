8dde1e6b9e41fea7f842246dd2ca38c9d783316c
dzimkaby <dzimkaby@gmail.com>
2020-04-28 18:31:57 +0300

    Загрузить ArrayChar.java в github

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,17 +2,18 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = true;
-        for (int index = 0; index < pref.length; index++) {
-            if (word[index] != pref[index]) {
-                result = false;
-            }
+    boolean result = true;
+    for (int index = 0; index < pref.length; index++) {
+        if (word[index] != pref[index]) {
+            result = false;
+            break;
         }
-        return result;
     }
+    return result;
+}
     public static void main(String[] args) {
-        char[] word = new char[] {'п', 'р', 'и' };
-        char[] pref = new char[] {'п', 'р', 'е' };
+        char[] word = new char[] {'п', 'р', 'е' ,'в', 'е', 'т'};
+        char[] pref = new char[] {'п', 'р', 'и' };
         boolean res = ArrayChar.startsWith(word, pref);
         System.out.println(res);
     }
